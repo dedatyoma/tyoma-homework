ca93ce4ceabbdb6844cfcc0463d5147c198c26ee
Артем Бурдикін <burdykinartem4@gmail.com>
2024-10-17 00:18:13 +0300

    ПЕРЕЗДАЧА

--- Final project/app/homepage/main.js
+++ Final project/app/homepage/main.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+  $('.frame3').slick({
+    infinite: true,      // Бесконечная прокрутка
+    slidesToShow: 3,      // Количество видимых слайдов
+    slidesToScroll: 1,    // Сколько слайдов прокручивать за раз
+    dots: true,           // Показывать индикаторы (точки)
+    arrows: true,         // Включить стрелки навигации
+    autoplay: true,       // Автопрокрутка
+    autoplaySpeed: 2000   // Интервал автопрокрутки (мс)
+  });
+});
